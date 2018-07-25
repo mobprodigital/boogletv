1d4963932ba3e7d94c7147359fca5ab1deb6b926
Gyan Parkash <gyan.p@morrisdigital.mobi>
2018-07-25 18:03:31 +0530

    home page slider

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { Video } from "../../models/video.model";
 
 @Component({
@@ -6,7 +6,9 @@ import { Video } from "../../models/video.model";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('imgSlider', { read: ElementRef }) imgSlider: ElementRef;
 
   selectedSlide: string = '';
 
@@ -22,6 +24,9 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+  }
+
   public showSlide(VidId: string) {
     this.selectedSlide = VidId;
   }
@@ -30,6 +35,7 @@ export class HomeComponent implements OnInit {
    * Temp function
    */
   private feedVideos(): void {
+
     this.LatestVideos = Array.from({ length: 3 }, (_, i: number) => {
 
       if (i === 0) {
@@ -44,6 +50,51 @@ export class HomeComponent implements OnInit {
       vdo.CreateDate = new Date();
       vdo.Thumbnails.Orignal = 'assets/images/home/movie' + (i + 1) + '.jpg';
       return vdo;
+    });
+
+    this.MostLikedVideos = Array.from({ length: 12 }, (_, i: number) => {
+      if (i === 0) {
+        this.selectedSlide = 'vid' + i;
+      }
+      let vdo = new Video();
+      vdo.Id = 'vid' + i;
+      vdo.Title = 'Video Title ' + i;
+      vdo.ViewsCount = 10;
+      vdo.LikesCount = 20;
+      vdo.CreateDate = new Date();
+      vdo.Thumbnails.Orignal = 'assets/images/home/movie1.jpg';
+      return vdo;
     })
   }
+
+  bannerSwipeLeft(slideindex: number): void {
+    if (slideindex >= (this.LatestVideos.length - 1)) {
+      this.showSlide('vid0');
+    }
+    else {
+      this.showSlide('vid' + (slideindex + 1));
+    }
+  }
+
+  bannerSwipeRight(slideindex: number): void {
+    if (slideindex <= 0) {
+      this.showSlide('vid' + (this.LatestVideos.length - 1));
+    }
+    else {
+      this.showSlide('vid' + (slideindex - 1));
+    }
+  }
+
+
+  imgSlide(slideTo: string) {
+    let slider: HTMLDivElement = this.imgSlider.nativeElement;
+    if (slideTo == 'left') {
+      slider.scroll({ left: (slider.scrollLeft + slider.clientWidth), behavior: 'smooth' });
+    }
+    else {
+      slider.scroll({ left: (slider.scrollLeft - slider.clientWidth), behavior: 'smooth' });
+    }
+
+  }
+
 }
