a72873ba0ba24d52e389cf633c5909e80525292e
Gyan Parkash <gyan.p@morrisdigital.mobi>
2018-07-26 14:22:01 +0530

    banner and image slider, mobile responsive and support touch device

--- src/app/interfaces/thumbnail.interface.ts
+++ src/app/interfaces/thumbnail.interface.ts
@@ -1,7 +1,19 @@
 
 export interface ThumbnailSize {
+    /**
+     * Orignal size of the thumbnail image
+     */
     Orignal: string;
+    /**
+     * 1200 x 600 width and height respectivly in pixels
+     */
     Large: string;
+    /**
+     * 600 x 300 width and height respectivly in pixels
+     */
     Medium: string;
+    /**
+     * 300 x 150 width and height respectivly in pixels
+     */
     small: string;
 }
